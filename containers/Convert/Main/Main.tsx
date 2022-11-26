c31f43c9e5c357ed706462376f0d71ea44174c8b
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-11-26 11:07:41 +0530

    add currency convert page

--- containers/Convert/Main/Main.tsx
+++ containers/Convert/Main/Main.tsx
@@ -1,11 +1,97 @@
 import { Currency } from "@/constants/currencyEnum";
+import { ConvertApiResponse } from "@/types/ConvertApi";
+import { useEffect, useState } from "react";
 import { SwapVerticalOutline } from "react-ionicons";
 
+type CurrencyConvertType = {
+    value: number;
+    type: string;
+};
+
 const Main = () => {
+    const currencies = Object.keys(Currency);
+
+    const [fromCur, setFromCur] = useState<CurrencyConvertType>({
+        value: 0,
+        type: currencies[0],
+    });
+    const [toCur, setToCur] = useState<CurrencyConvertType>({
+        value: 0,
+        type: currencies[14],
+    });
+
+    const [convRate, setConvRate] = useState<number>(0);
+
+    const fetchConvRate = async (from: string, to: string, value: number) => {
+        const res = await fetch(`/api/convert`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                from: from,
+                to: to,
+                value: value,
+            }),
+        });
+
+        const data = await res.json();
+
+        return data;
+    };
+
+    useEffect(() => {
+        const defaultConvRate = async () => {
+            const response: ConvertApiResponse = await fetchConvRate(
+                fromCur.type,
+                toCur.type,
+                1
+            );
+
+            setConvRate(response.rate);
+        };
+
+        defaultConvRate();
+    }, [fromCur, toCur]);
+
+    const selectCurrency = (
+        conversion: "from" | "to",
+        currency: string
+    ): void => {
+        if (conversion == "from") {
+            setFromCur((prev) => {
+                prev.type = currency;
+                return prev;
+            });
+        } else {
+            setToCur((prev) => {
+                prev.type = currency;
+                return prev;
+            });
+        }
+    };
+
+    const handleSubmit = async (e: any): Promise<void> => {
+        e.preventDefault();
+
+        const response: ConvertApiResponse = await fetchConvRate(
+            fromCur.type,
+            toCur.type,
+            fromCur.value
+        );
+
+        setToCur((prev) => {
+            prev.value = response.rate;
+            return prev;
+        });
+
+        console.log(toCur);
+    };
+
     return (
         <>
             <div id="appCapsule">
-                <form>
+                <form onSubmit={(e) => handleSubmit(e)}>
                     <div className="section mt-4">
                         <div className="card">
                             <div className="card-body">
@@ -17,30 +103,53 @@ const Main = () => {
                                         >
                                             From
                                         </label>
-                                        <div className="exchange-wallet-info">
+                                        {/* <div className="exchange-wallet-info">
                                             Balance :{" "}
                                             <strong> 5.506,12 USD</strong>
-                                        </div>
+                                        </div> */}
                                     </div>
                                     <div className="exchange-group">
                                         <div className="input-col">
                                             <input
-                                                type="text"
+                                                type="num"
                                                 className="form-control form-control-lg pe-0 border-0"
                                                 id="fromAmount"
                                                 placeholder="0"
+                                                pattern="[0-9]*"
+                                                inputMode="numeric"
                                                 maxLength={10}
+                                                onChange={(e) =>
+                                                    setFromCur((prev) => {
+                                                        prev.value = parseFloat(
+                                                            e.target.value
+                                                        );
+                                                        return prev;
+                                                    })
+                                                }
                                             />
                                         </div>
                                         <div className="select-col">
                                             <select className="form-select form-select-lg currency">
-                                                {/* {Currency} */}
-                                                <option value="USD" selected>
-                                                    USD
-                                                </option>
-                                                <option value="BTC">BTC</option>
-                                                <option value="ETH">ETH</option>
-                                                <option value="ADA">ADA</option>
+                                                {currencies.map(
+                                                    (key, index) => (
+                                                        <option
+                                                            key={index}
+                                                            selected={
+                                                                key ==
+                                                                fromCur.type
+                                                            }
+                                                            // value={key}
+                                                            onClick={() =>
+                                                                selectCurrency(
+                                                                    "from",
+                                                                    key
+                                                                )
+                                                            }
+                                                        >
+                                                            {key}
+                                                        </option>
+                                                    )
+                                                )}
                                             </select>
                                         </div>
                                     </div>
@@ -68,10 +177,10 @@ const Main = () => {
                                         >
                                             To
                                         </label>
-                                        <div className="exchange-wallet-info">
+                                        {/* <div className="exchange-wallet-info">
                                             Balance :{" "}
                                             <strong> 0,504 BTC</strong>
-                                        </div>
+                                        </div> */}
                                     </div>
                                     <div className="exchange-group">
                                         <div className="input-col">
@@ -79,18 +188,33 @@ const Main = () => {
                                                 type="text"
                                                 className="form-control form-control-lg pe-0 border-0"
                                                 id="toAmount"
-                                                placeholder="0"
+                                                value={toCur.value}
+                                                readOnly={true}
                                                 maxLength={10}
                                             />
                                         </div>
                                         <div className="select-col">
                                             <select className="form-select form-select-lg currency">
-                                                <option value="USD">USD</option>
-                                                <option value="BTC" selected>
-                                                    BTC
-                                                </option>
-                                                <option value="ETH">ETH</option>
-                                                <option value="ADA">ADA</option>
+                                                {currencies.map(
+                                                    (key, index) => (
+                                                        <option
+                                                            key={index}
+                                                            selected={
+                                                                key ==
+                                                                toCur.type
+                                                            }
+                                                            // value={key}
+                                                            onClick={() =>
+                                                                selectCurrency(
+                                                                    "to",
+                                                                    key
+                                                                )
+                                                            }
+                                                        >
+                                                            {key}
+                                                        </option>
+                                                    )
+                                                )}
                                             </select>
                                         </div>
                                     </div>
@@ -102,11 +226,12 @@ const Main = () => {
                     <div className="section mt-2">
                         <div className="row fontsize-caption">
                             <div className="col">
-                                <b>1 BTC</b> = 39.019,09 USD
+                                <b>1 {fromCur.type}</b> = {convRate}{" "}
+                                {toCur.type}
                             </div>
-                            <div className="col text-end">
+                            {/* <div className="col text-end">
                                 Commission rate : <b>2%</b>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
 
@@ -114,8 +239,9 @@ const Main = () => {
                         <button
                             type="submit"
                             className="btn btn-primary btn-block btn-lg"
+                            onSubmit={(e) => handleSubmit(e)}
                         >
-                            Continue
+                            Check
                         </button>
                     </div>
                 </form>
