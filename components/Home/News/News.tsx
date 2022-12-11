7ecdbc5fdf73d30cd05660b3f4bf885ac3fd627d
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-12-11 17:47:25 +0530

    finish home page

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -1,7 +1,36 @@
+import ComponentLoaderPrimary from "@/components/ComponentLoader/ComponentLoaderPrimary";
 import Link from "@/helpers/wrappers/Link/Link";
+import { gql, useQuery } from "@apollo/client";
+import { News } from "@prisma/client";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 
+const placeholderImage = "/assets/img/icon/default-image.jpg";
+
+const GET_NEWS = gql`
+    query Query($page: Int, $itemsPerPage: Int) {
+        getCurrency {
+            currencyName
+        }
+        getAllNews(page: $page, itemsPerPage: $itemsPerPage) {
+            title
+            imageUrl
+            id
+        }
+    }
+`;
+
 const News = () => {
+    const { loading, error, data } = useQuery(GET_NEWS, {
+        variables: {
+            page: 1,
+            itemsPerPage: 4,
+        },
+    });
+
+    if (data) {
+        console.log(data);
+    }
+
     return (
         <>
             <div className="section full mt-4 mb-3">
@@ -28,74 +57,56 @@ const News = () => {
                     }}
                 >
                     <SplideTrack className="splide__track">
-                        <SplideSlide className="splide__slide">
-                            <a href="app-blog-post.html">
-                                <div className="blog-card">
-                                    <img
-                                        src="/assets/img/sample/photo/1.jpg"
-                                        alt="image"
-                                        className="imaged w-100"
-                                    />
-                                    <div className="text">
-                                        <h4 className="title">
-                                            What will be the value of bitcoin in
-                                            the next...
-                                        </h4>
-                                    </div>
-                                </div>
-                            </a>
-                        </SplideSlide>
-
-                        <SplideSlide className="splide__slide">
-                            <a href="app-blog-post.html">
-                                <div className="blog-card">
-                                    <img
-                                        src="/assets/img/sample/photo/2.jpg"
-                                        alt="image"
-                                        className="imaged w-100"
-                                    />
-                                    <div className="text">
-                                        <h4 className="title">
-                                            Rules you need to know in business
-                                        </h4>
-                                    </div>
-                                </div>
-                            </a>
-                        </SplideSlide>
-
-                        <SplideSlide className="splide__slide">
-                            <a href="app-blog-post.html">
-                                <div className="blog-card">
-                                    <img
-                                        src="/assets/img/sample/photo/3.jpg"
-                                        alt="image"
-                                        className="imaged w-100"
-                                    />
-                                    <div className="text">
-                                        <h4 className="title">
-                                            10 easy ways to save your money
-                                        </h4>
-                                    </div>
-                                </div>
-                            </a>
-                        </SplideSlide>
-
-                        <SplideSlide className="splide__slide">
-                            <a href="app-blog-post.html">
-                                <div className="blog-card">
-                                    <img
-                                        src="/assets/img/sample/photo/4.jpg"
-                                        alt="image"
-                                        className="imaged w-100"
-                                    />
-                                    <div className="text">
-                                        <h4 className="title">
-                                            Follow the financial agenda with...
-                                        </h4>
-                                    </div>
-                                </div>
-                            </a>
-                        </SplideSlide>
+                        {loading ? (
+                            <ComponentLoaderPrimary />
+                        ) : (
+                            <>
+                                {data.getAllNews.map((news: News) => (
+                                    <SplideSlide
+                                        key={news.id}
+                                        className="splide__slide"
+                                    >
+                                        <Link
+                                            href={`/news/${encodeURIComponent(
+                                                news.id
+                                            )}`}
+                                        >
+                                            <div className="blog-card">
+                                                <img
+                                                    src={
+                                                        !news.imageUrl
+                                                            ? placeholderImage
+                                                            : news.imageUrl
+                                                    }
+                                                    alt="image"
+                                                    onError={({
+                                                        currentTarget,
+                                                    }) => {
+                                                        currentTarget.onerror =
+                                                            null; // prevents looping
+                                                        currentTarget.src =
+                                                            placeholderImage;
+                                                    }}
+                                                    className="imaged w-100"
+                                                />
+                                                <div className="text">
+                                                    <h4 className="title">
+                                                        {news.title.slice(
+                                                            0,
+                                                            50
+                                                        ) +
+                                                            (news.title
+                                                                .length >= 50
+                                                                ? "..."
+                                                                : "")}
+                                                    </h4>
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </SplideSlide>
+                                ))}
+                            </>
+                        )}
                     </SplideTrack>
                 </Splide>
             </div>
