40ef56b339c075422d084a506aeb652f758d867a
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-12-11 23:38:01 +0530

    fix build error

---
+++ graphql/types/Query.ts
@@ -0,0 +1,129 @@
+import { idArg, intArg, nonNull, queryType, stringArg } from "nexus";
+import { getAllAccountsResolver } from "../resolvers/Account/getAllAccountsResolver";
+import { getAllBillsResolver } from "../resolvers/Bill/getAllBillsResolver";
+import { getChatResponseResolver } from "../resolvers/Chat/getChatResponseResolver";
+import { getCurrencyResolver } from "../resolvers/Currency/getCurrencyResolver";
+import { getCurrentExpenseStatusResolver } from "../resolvers/ExpenseStatus/getCurrentExpenseStatusResolver";
+import { getAllGoalsResolver } from "../resolvers/Goal/getAllGoalsResolver";
+import { healthCheckResolver } from "../resolvers/HealthCheckResolver";
+import { getAllLogsResolver } from "../resolvers/Log/getAllLogsResolver";
+import { getAllNewsResolver } from "../resolvers/News/getAllNewsResolver";
+import { getNewsByIdResolver } from "../resolvers/News/getNewsByIdResolver";
+import { getAllTxnsResolver } from "../resolvers/Transaction/getAllTxnsResolver";
+import { Currency } from "./TypeDefs/Currency";
+import {
+    News,
+    Test,
+    Chat,
+    Account,
+    Bill,
+    Goal,
+    Log,
+    Transaction,
+    ExpenseStatus,
+} from "./TypeDefs/index";
+
+export const Query = queryType({
+    definition(t) {
+        t.field("testing", {
+            type: Test,
+            description: "Health Check GraphQL resolver.",
+            args: {},
+            resolve: healthCheckResolver,
+        });
+
+        t.field("getCurrency", {
+            type: Currency,
+            description: "Get the selected currency of the user.",
+            args: {},
+            resolve: getCurrencyResolver,
+        });
+
+        t.list.field("getAllNews", {
+            type: News,
+            description: "Gets all the news articles.",
+            args: {
+                page: intArg(),
+                itemsPerPage: intArg(),
+            },
+            resolve: getAllNewsResolver,
+        });
+
+        t.field("getNewsById", {
+            type: News,
+            description: "Get the news by ID",
+            args: {
+                id: nonNull(idArg()),
+            },
+            resolve: getNewsByIdResolver,
+        });
+
+        t.field("getChatResponse", {
+            type: Chat,
+            description: "Chat with an AI!",
+            args: {
+                message: nonNull(stringArg()),
+                sender: nonNull(stringArg()),
+            },
+            resolve: getChatResponseResolver,
+        });
+
+        t.list.field("getAllAccounts", {
+            type: Account,
+            description: "Get all the accounts of a particular user",
+            args: {
+                page: intArg(),
+                itemsPerPage: intArg(),
+            },
+            resolve: getAllAccountsResolver,
+        });
+
+        t.list.field("getAllBills", {
+            type: Bill,
+            description: "Get all the bills of a particular user",
+            args: {
+                page: intArg(),
+                itemsPerPage: intArg(),
+            },
+            resolve: getAllBillsResolver,
+        });
+
+        t.list.field("getAllGoals", {
+            type: Goal,
+            description: "Get all the goals of a particular user",
+            args: {
+                page: intArg(),
+                itemsPerPage: intArg(),
+            },
+            resolve: getAllGoalsResolver,
+        });
+
+        t.list.field("getAllLogs", {
+            type: Log,
+            description: "Get all the logs of a particular user",
+            args: {
+                page: intArg(),
+                itemsPerPage: intArg(),
+            },
+            resolve: getAllLogsResolver,
+        });
+
+        t.list.field("getAllTxns", {
+            type: Transaction,
+            description: "Get all the transactions of a particular user",
+            args: {
+                page: intArg(),
+                itemsPerPage: intArg(),
+            },
+            resolve: getAllTxnsResolver,
+        });
+
+        t.field("getCurrentExpenseStatus", {
+            type: ExpenseStatus,
+            description:
+                "Get current status of al the expenses like balance, savings, etc.",
+            args: {},
+            resolve: getCurrentExpenseStatusResolver,
+        });
+    },
+});
---
+++ graphql/types/TypeDefs/User.ts
@@ -0,0 +1,90 @@
+import { GraphQlContextType } from "@/types/GraphQL";
+import { objectType } from "nexus";
+import { Account } from "./Account";
+import { Bill } from "./Bill";
+import { Goal } from "./Goal";
+import { Log } from "./Log";
+import { Profile } from "./Profile";
+import { Transaction } from "./Transaction";
+
+export const User = objectType({
+    name: "User",
+    definition(t) {
+        t.string("id");
+        t.string("name");
+        t.string("email");
+        t.field("Profile", {
+            type: Profile,
+            resolve: async (parent, args, ctx: GraphQlContextType, info) => {
+                return await ctx.prisma.profile.findFirst({
+                    where: {
+                        user: {
+                            id: parent.id,
+                        },
+                    },
+                });
+            },
+        });
+        t.list.field("Account", {
+            type: Account,
+            resolve: async (parent, args, ctx: GraphQlContextType, info) => {
+                return await ctx.prisma.bankAccount.findMany({
+                    where: {
+                        user: {
+                            id: parent.id,
+                        },
+                    },
+                });
+            },
+        });
+        t.list.field("Bill", {
+            type: Bill,
+            resolve: async (parent, args, ctx: GraphQlContextType, info) => {
+                return await ctx.prisma.bill.findMany({
+                    where: {
+                        user: {
+                            id: parent.id,
+                        },
+                    },
+                });
+            },
+        });
+        t.list.field("Goal", {
+            type: Goal,
+            resolve: async (parent, args, ctx: GraphQlContextType, info) => {
+                return await ctx.prisma.goal.findMany({
+                    where: {
+                        user: {
+                            id: parent.id,
+                        },
+                    },
+                });
+            },
+        });
+        t.list.field("Log", {
+            type: Log,
+            resolve: async (parent, args, ctx: GraphQlContextType, info) => {
+                return await ctx.prisma.log.findMany({
+                    where: {
+                        user: {
+                            id: parent.id,
+                        },
+                    },
+                });
+            },
+        });
+        t.list.field("Transaction", {
+            type: Transaction,
+            // @ts-ignore
+            resolve: async (parent, args, ctx: GraphQlContextType, info) => {
+                return await ctx.prisma.transaction.findMany({
+                    where: {
+                        user: {
+                            id: parent.id,
+                        },
+                    },
+                });
+            },
+        });
+    },
+});
