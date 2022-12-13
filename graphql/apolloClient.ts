d47ca0fe102839aa3180a0973c1b232eb1b71601
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-12-13 22:18:30 +0530

    fix build error 20

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -10,7 +10,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject>;
 function createApolloClient() {
     return new ApolloClient({
         link: new HttpLink({
-            uri: "https://ecospense-git-dev-k4u5h4l.vercel.app/api/graphql",
+            uri: "/api/graphql",
         }),
         cache: new InMemoryCache(),
     });
