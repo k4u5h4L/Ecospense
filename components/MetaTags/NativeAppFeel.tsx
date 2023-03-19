01e5310f6878af56cb731e20db0a0b03fe0eb127
k4u5h4L <kaushal.v.bhat@gmail.com>
2023-03-19 12:59:39 +0530

    fix user scaling

--- components/MetaTags/NativeAppFeel.tsx
+++ components/MetaTags/NativeAppFeel.tsx
@@ -11,13 +11,14 @@ const NativeAppFeel = () => {
             <meta name="theme-color" content="#6236ff" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta
-                name="viewport"
-                content="initial-scale=1, viewport-fit=cover, user-scalable=no"
-            />
-            <meta
                 name="apple-mobile-web-app-status-bar-style"
                 content="default"
             />
+            <meta
+                name="viewport"
+                content="width=device-width, viewport-fit=cover, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            />
+            <meta name="HandheldFriendly" content="true" />
         </>
     );
 };
